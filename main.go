7826f6c2d4415fa99afae1c0793bd2b58cfe3a23
Nicolas <nicolashereigo@gmail.com>
2022-03-03 19:42:44 +0300

    Updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"printer/api/jobs"
+	"printer/interfaces"
+	"printer/job"
+	"printer/jobqueue"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	q := jobqueue.NewJobQueue()
+
+	createJob := func() interfaces.Job {
+		return &job.Job{}
+	}
+
+	jobsApi := jobs.NewRouter(q, createJob)
+	fmt.Println("The server starting on localhost:7777")
+	http.ListenAndServe(":7777", jobsApi)
 }

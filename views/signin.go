49aef6a848faad1c9192d54abf7d4ca3ade34bad
Nicolas <nicolashereigo@gmail.com>
2022-03-07 13:55:34 +0300

    Added mock views

---
+++ views/signin.go
@@ -0,0 +1,27 @@
+package views
+
+import (
+	"fmt"
+	"net/http"
+	"printer/interfaces"
+)
+
+func BuildSignin(templates interfaces.Templates) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			signinTemplate := templates.GetSignin()
+
+			// data query logic
+			// to be added in the future
+			// mock data
+			data := struct {
+				Greeting string
+			}{
+				Greeting: "Hello",
+			}
+
+			err := signinTemplate.Execute(w, data)
+			fmt.Println(err)
+		},
+	)
+}

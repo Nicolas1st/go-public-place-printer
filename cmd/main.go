54a13e237013628146bfbc771b390413fd26057e
Nicolas <nicolashereigo@gmail.com>
2022-03-11 22:54:00 +0300

    Updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"printer/api/jobs"
 	"printer/interfaces"
 	"printer/job"
 	"printer/jobqueue"
+	"printer/pages"
+	"printer/views"
 )
+
+var host = "localhost:7777"
 
 func main() {
 	q := jobqueue.NewJobQueue()
+	jobsApi := jobs.NewRouter(q,
+		func() interfaces.Job {
+			return &job.Job{}
+		},
+	)
 
-	createJob := func() interfaces.Job {
-		return &job.Job{}
-	}
+	templates := pages.NewTemplates("../static/html")
+	views := views.NewRouter(templates)
 
-	jobsApi := jobs.NewRouter(q, createJob)
-	fmt.Println("The server starting on localhost:7777")
-	http.ListenAndServe(":7777", jobsApi)
+	http.Handle("/jobs", jobsApi)
+	http.Handle("/pages", views)
+
+	fmt.Printf("The server has start on %v\n", host)
+	http.ListenAndServe(host, views)
 }

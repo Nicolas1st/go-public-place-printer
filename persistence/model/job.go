a1409a8a09ca3f5446e8bc5600e6238da7bdf1dc
Nicolas <nicolashereigo@gmail.com>
2022-04-18 18:02:42 +0300

    Added JobStatus type to model

--- persistence/model/job.go
+++ persistence/model/job.go
@@ -1,18 +1,31 @@
 package model
 
+type JobStatus int
+
+const (
+	StatusCancelled = iota
+	StatusToBeDone
+)
+
 type JobID uint
 
 type Job struct {
 	ID       JobID
 	FilePath string
 	Username string
+	Status   JobStatus
 }
 
 func NewJob(filepath, username string) *Job {
 	return &Job{
 		FilePath: filepath,
 		Username: username,
+		Status:   StatusToBeDone,
 	}
+}
+
+func (j *Job) CancelJob() {
+	j.Status = StatusCancelled
 }
 
 func (j *Job) SetID(id JobID) {

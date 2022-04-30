830c4ce2a4bd271c765838aec170a78d3a64fbb9
Nicolas <nicolashereigo@gmail.com>
2022-04-30 16:07:42 +0300

    Added in-memory sesion storage

---
+++ persistence/session/session.go
@@ -0,0 +1,78 @@
+package session
+
+import (
+	"printer/persistence/model"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type SessionStorage struct {
+	sessions     map[string]*model.Session
+	lastPurgedAt time.Time
+}
+
+func NewSessionStorage() *SessionStorage {
+	return &SessionStorage{
+		sessions:     make(map[string]*model.Session),
+		lastPurgedAt: time.Now(),
+	}
+}
+
+func (storage *SessionStorage) purgeFromExpiredSessions() {
+	for token, session := range storage.sessions {
+		if session.IsExpired() {
+			delete(storage.sessions, token)
+		}
+	}
+}
+
+func (storage *SessionStorage) StoreSession(session *model.Session) (string, time.Time) {
+	// to avoid memory leaks the session are being purged
+	// It's done every expiry perdiod of one cookies elapses
+	// the persiod is defined in session.go
+	if time.Now().After(storage.lastPurgedAt.Add(model.ExpiryPeriod)) {
+		storage.purgeFromExpiredSessions()
+	}
+
+	defer func() {
+		// the token generator function for some reason can throw a panic
+		// it's an inner implementation issue(?), so it's being handled here
+		recover()
+	}()
+
+	// in case the already used token is being generated,
+	// it's almost impossible but can happen anyway
+	var sessionToken string
+	for {
+		sessionToken = uuid.NewString()
+		if _, alreadyExists := storage.sessions[sessionToken]; !alreadyExists {
+			break
+		}
+	}
+
+	// storing the session in memory
+	storage.sessions[sessionToken] = session
+
+	return sessionToken, session.ExpiresAt
+}
+
+func (storage *SessionStorage) RemoveSession(sessionToken string) {
+	delete(storage.sessions, sessionToken)
+}
+
+// IsSessionValid checks whether the session is valid,
+// it checks if it exists and is not too old
+func (storage *SessionStorage) IsSessionValid(sessionToken string) bool {
+	session, exists := storage.sessions[sessionToken]
+	if !exists {
+		return false
+	}
+
+	if session.IsExpired() {
+		storage.RemoveSession(sessionToken)
+		return false
+	}
+
+	return true
+}

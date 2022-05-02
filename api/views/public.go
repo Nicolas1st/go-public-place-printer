b71197799d183bbcfe5723d41b928e8cf72ad0d3
Nicolas <nicolashereigo@gmail.com>
2022-05-02 14:15:08 +0300

    Moved signup and login views to public views

--- api/views/public.go
+++ api/views/public.go
@@ -14,3 +14,13 @@ func BuildSignupView(page *pages.SignupPage) http.HandlerFunc {
 		}
 	}
 }
+
+func BuildLoginView(page *pages.LoginPage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := page.Execute(w, struct{ Greeting string }{Greeting: "Hello"})
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+}

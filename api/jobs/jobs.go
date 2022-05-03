4f68488084e1e0cf846ecf01b49292384592f7e3
Nicolas <nicolashereigo@gmail.com>
2022-05-03 15:44:15 +0300

    Refactored jobs router to be able to pass through auth middelware

--- api/jobs/jobs.go
+++ api/jobs/jobs.go
@@ -2,9 +2,11 @@ package jobs
 
 import "net/http"
 
-func NewRouter(jobq jobqInterface, filer filerInterface) http.Handler {
+func NewRouter(jobq jobqInterface, filer filerInterface) *http.ServeMux {
+	router := http.NewServeMux()
+
 	resource := NewJobsResource(jobq, filer)
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
 			resource.SubmitJob(w, r)
@@ -12,4 +14,6 @@ func NewRouter(jobq jobqInterface, filer filerInterface) http.Handler {
 			resource.CancelJob(w, r)
 		}
 	})
+
+	return router
 }

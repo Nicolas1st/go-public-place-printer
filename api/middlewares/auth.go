93441a262f00e7f6585a00a2ee9aabb28acdd71e
Nicolas <nicolashereigo@gmail.com>
2022-05-03 14:08:35 +0300

    Renamed middlewares

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -6,13 +6,12 @@ import (
 )
 
 type SessionStorageInterface interface {
-	IsSessionValid(sessionToken string) bool
 }
 
-// ForbidForAuthenticated - prevents non-authenticated users from making requests to certain enpoints (parameter next)
+// OnlyAuthenticated - prevents non-authenticated users from making requests to certain enpoints (parameter next)
 // based on whether the auth cookie is present
 // and the validity of the session
-func ForbidForNonAuthenticated(sessionStorage SessionStorageInterface, next *http.ServeMux, redirectTo http.HandlerFunc) http.HandlerFunc {
+func OnlyAuthenticated(sessionStorage SessionStorageInterface, next *http.ServeMux, redirectTo http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie(auth.AuthCookieName)
 
@@ -33,9 +32,9 @@ func ForbidForNonAuthenticated(sessionStorage SessionStorageInterface, next *htt
 	}
 }
 
-// ForbidForAuthenticated - prevents authenticated users from making requests to certain enpoints (parameter next)
+// OnlyAnonymous - prevents authenticated users from making requests to certain enpoints (parameter next)
 // based on whether the auth cookie is present
-func ForbidForAuthenticated(next *http.ServeMux, redirectTo http.HandlerFunc) http.HandlerFunc {
+func OnlyAnonymous(next *http.ServeMux, redirectTo http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		_, cookieNotPresentErr := r.Cookie(auth.AuthCookieName)
 

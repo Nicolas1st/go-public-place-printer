0b817121bd94bda88e09ca344f41b791775f9efe
Nicolas <nicolashereigo@gmail.com>
2022-05-04 11:29:29 +0300

    Refactored auth middleware

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -7,50 +7,59 @@ import (
 	"printer/persistence/model"
 )
 
-type contextSessionKey string
-
 var ContextSessionKey contextSessionKey = "contextSessionKey"
+
+type contextSessionKey string
 
 type SessionStorageInterface interface {
 	GetSessionByToken(sessionToken string) (*model.Session, error)
 }
 
-// OnlyAuthenticated - prevents non-authenticated users from making requests to certain enpoints (parameter next)
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// BuildOnlyAuthenticatedMiddleware - build middelware that
+// prevents non-authenticated users from making requests to certain enpoints (parameter next)
 // based on whether the auth cookie is present
 // and the validity of the session
-func OnlyAuthenticated(sessionStorage SessionStorageInterface, next *http.ServeMux, redirectTo http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// check if auth cookie is present
-		cookie, err := r.Cookie(auth.AuthCookieName)
-		if err != nil {
-			redirectTo(w, r)
-			return
-		}
+func BuildOnlyAuthenticatedMiddleware(sessionStorage SessionStorageInterface, redirectTo http.HandlerFunc) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			// check if auth cookie is present
+			cookie, err := r.Cookie(auth.AuthCookieName)
+			if err != nil {
+				redirectTo(w, r)
+				return
+			}
 
-		// checking whether a valid sesion exists for the provided
-		session, err := sessionStorage.GetSessionByToken(cookie.Value)
-		if err != nil {
-			redirectTo(w, r)
-		}
+			// checking whether a valid sesion exists for the provided
+			session, err := sessionStorage.GetSessionByToken(cookie.Value)
+			if err != nil {
+				redirectTo(w, r)
+				return
+			}
 
-		// storing the session in the context
-		r = r.WithContext(context.WithValue(r.Context(), ContextSessionKey, session))
-		next.ServeHTTP(w, r)
+			// storing the session in the context
+			r = r.WithContext(context.WithValue(r.Context(), ContextSessionKey, session))
+			next.ServeHTTP(w, r)
+		}
 	}
 }
 
-// OnlyAnonymous - prevents authenticated users from making requests to certain enpoints (parameter next)
+// BuildOnlyAnonymousMiddleware - builds middleware that
+// prevents authenticated users from making requests to certain enpoints (parameter next)
 // based on whether the auth cookie is present
-func OnlyAnonymous(next *http.ServeMux, redirectTo http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		_, err := r.Cookie(auth.AuthCookieName)
+func BuildOnlyAnonymousMiddleware(redirectTo http.HandlerFunc) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			_, err := r.Cookie(auth.AuthCookieName)
 
-		if err == nil {
-			// non authenticated don't have auth cookie
-			redirectTo(w, r)
-		} else {
-			// authenticated users have one
-			next.ServeHTTP(w, r)
+			if err == nil {
+				// non authenticated don't have auth cookie
+				redirectTo(w, r)
+			} else {
+				// authenticated users have one
+				next.ServeHTTP(w, r)
+			}
 		}
 	}
 }

5ad09e4eb2a4169c1b86a229a9250eb4d2ff4fd9
Nicolas <nicolashereigo@gmail.com>
2022-05-05 10:48:16 +0300

    Simplified router

--- api/jobs/jobs.go
+++ api/jobs/jobs.go
@@ -2,18 +2,15 @@ package jobs
 
 import "net/http"
 
-func NewRouter(jobq jobqInterface, filer filerInterface) *http.ServeMux {
-	router := http.NewServeMux()
-
+func NewRouter(jobq jobqInterface, filer filerInterface) http.HandlerFunc {
 	resource := NewJobsResource(jobq, filer)
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
 			resource.SubmitJob(w, r)
 		case http.MethodDelete:
 			resource.CancelJob(w, r)
 		}
-	})
-
-	return router
+	}
 }

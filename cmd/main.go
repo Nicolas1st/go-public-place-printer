0596232fce0867de11fcdc362e9b0dcedc20ed6d
Nicolas <nicolashereigo@gmail.com>
2022-05-05 11:12:55 +0300

    Updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,81 @@ package main
 
 import (
 	"net/http"
+	"printer/api/auth"
 	"printer/api/jobs"
+	"printer/api/middlewares"
+	"printer/api/register"
 	"printer/api/views"
-	"printer/api/views/pages"
+	"printer/persistence/db"
 	"printer/persistence/filer"
 	"printer/persistence/jobq"
+	"printer/persistence/session"
+	"printer/worker"
 )
 
 func main() {
+	// config
 	server := &http.Server{
 		Addr: "127.0.0.1:8880",
 	}
+	dsn := "host=localhost user=postgres password=password dbname=stuff port=5432 sslmode=disable"
 
+	// persistence layer
+	db := db.NewDatabase(dsn)
 	jobq := jobq.NewJobQueue()
-	filer := filer.NewFiler("./files", 2<<30)
+	filer := filer.NewFiler("../files", 2<<30)
+	sessionStorage := session.NewSessionStorage()
 
-	views := views.NewRouter(*pages.NewPages("./web/html"), jobq)
-	http.Handle("/", views)
+	// serving static files
+	files := http.FileServer(http.Dir("../web"))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
 
+	// build middlewares for http handlers
+	onlyAuth := middlewares.BuildOnlyAuthenticatedMiddleware(
+		sessionStorage,
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Redirect to page for non authenticated users"))
+		},
+	)
+
+	onlyNotAuth := middlewares.BuildOnlyAnonymousMiddleware(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Redirect to page for authenticated users"))
+		},
+	)
+
+	// setting up views
+	views := views.NewViews("../web/html")
+
+	// not protected routes
+	http.HandleFunc("/", onlyNotAuth(views.Login))
+	http.HandleFunc("/login", onlyNotAuth(views.Login))
+	http.HandleFunc("/signup", onlyNotAuth(views.Signup))
+
+	// protected routes
+	http.HandleFunc("/submit-file", onlyAuth(views.SubmitFile))
+
+	// jobs api to submit files to be printed
 	jobsApi := jobs.NewRouter(jobq, filer)
-	http.Handle("/jobs", jobsApi)
+	http.Handle("/jobs", onlyAuth(jobsApi))
+
+	// registration
+	userController := register.UserController{
+		DB: db,
+	}
+	http.HandleFunc("/register", userController.CreateNewUser)
+
+	// authenication
+	auth := auth.NewAuthRouter(
+		sessionStorage,
+		db,
+		views.SubmitFile,
+		views.Login,
+	)
+	http.Handle("/auth/", http.StripPrefix("/auth", auth))
+
+	worker := worker.NewWorker(jobq, filer)
+	worker.Start()
 
 	server.ListenAndServe()
 }

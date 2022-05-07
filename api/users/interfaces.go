ade09ba4ffdc1569a4a8aa7c5688c33078fc82ae
Nicolas <nicolashereigo@gmail.com>
2022-05-07 15:55:45 +0300

    Added functions to users api database interface

--- api/users/interfaces.go
+++ api/users/interfaces.go
@@ -3,7 +3,13 @@ package users
 import "printer/persistence/model"
 
 type databaseInterface interface {
+	// user managment
 	DeleteUserByID(id uint) error
 	GetAllUsers() []model.User
 	GetUserByID(id uint) (*model.User, error)
+
+	// permissions
+	SetPagesPerMonth(id uint, pagesPerMonth int) error
+	AllowUsingPrinter(userID uint) error
+	ForbidUsingPrinter(userID uint) error
 }

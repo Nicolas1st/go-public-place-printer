c5cf501aef933028c03f0ee80f6ca9f02b22f320
Nicolas <nicolashereigo@gmail.com>
2022-05-10 19:58:13 +0300

    Added redirects to register function

--- api/register/register.go
+++ api/register/register.go
@@ -10,31 +10,39 @@ type UserController struct {
 	DB DatabaseInterface
 }
 
-func (controller UserController) CreateNewUser(w http.ResponseWriter, r *http.Request) {
-	username := r.PostFormValue("username")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-	repeatPassword := r.PostFormValue("repeatPassword")
+func (controller UserController) BuildCreateNewUser(
+	redirectToOnSuccess http.HandlerFunc,
+	redirectToOnFail http.HandlerFunc,
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username := r.PostFormValue("username")
+		email := r.PostFormValue("email")
+		password := r.PostFormValue("password")
+		repeatPassword := r.PostFormValue("repeatPassword")
 
-	// making sure the password are the same on the server side
-	if password != repeatPassword {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		// making sure the password are the same on the server side
+		if password != repeatPassword {
+			redirectToOnFail(w, r)
+			return
+		}
 
-	// hashing the password
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), 16)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		// hashing the password
+		passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), 16)
+		if err != nil {
+			// add flash messages later
+			redirectToOnFail(w, r)
+			return
+		}
 
-	// creating new user
-	err = controller.DB.CreateNewUser(username, email, string(passwordHash))
+		// creating new user
+		err = controller.DB.CreateNewUser(username, email, string(passwordHash))
 
-	// abort if it was possible to create the user account
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		// abort if it was possible to create the user account
+		if err != nil {
+			redirectToOnFail(w, r)
+			return
+		}
+
+		redirectToOnSuccess(w, r)
 	}
 }

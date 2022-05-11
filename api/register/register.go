d66d70561773e899d75c27f446e731007f6135b7
Nicolas <nicolashereigo@gmail.com>
2022-05-11 16:38:48 +0300

    Refactored handler to use it with redirection middleware

--- api/register/register.go
+++ api/register/register.go
@@ -1,6 +1,7 @@
 package register
 
 import (
+	"errors"
 	"net/http"
 
 	"golang.org/x/crypto/bcrypt"
@@ -10,39 +11,30 @@ type UserController struct {
 	DB DatabaseInterface
 }
 
-func (controller UserController) BuildCreateNewUser(
-	redirectToOnSuccess http.HandlerFunc,
-	redirectToOnFail http.HandlerFunc,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		username := r.PostFormValue("username")
-		email := r.PostFormValue("email")
-		password := r.PostFormValue("password")
-		repeatPassword := r.PostFormValue("repeatPassword")
+func (controller UserController) CreateNewUser(w http.ResponseWriter, r *http.Request) error {
+	username := r.PostFormValue("username")
+	email := r.PostFormValue("email")
+	password := r.PostFormValue("password")
+	repeatPassword := r.PostFormValue("repeatPassword")
 
-		// making sure the password are the same on the server side
-		if password != repeatPassword {
-			redirectToOnFail(w, r)
-			return
-		}
-
-		// hashing the password
-		passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), 16)
-		if err != nil {
-			// add flash messages later
-			redirectToOnFail(w, r)
-			return
-		}
-
-		// creating new user
-		err = controller.DB.CreateNewUser(username, email, string(passwordHash))
-
-		// abort if it was possible to create the user account
-		if err != nil {
-			redirectToOnFail(w, r)
-			return
-		}
-
-		redirectToOnSuccess(w, r)
+	// making sure the password are the same on the server side
+	if password != repeatPassword {
+		return errors.New("password do not match")
 	}
+
+	// hashing the password
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), 16)
+	if err != nil {
+		return errors.New("could not hash the password")
+	}
+
+	// creating new user
+	err = controller.DB.CreateNewUser(username, email, string(passwordHash))
+
+	// abort if it was possible to create the user account
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

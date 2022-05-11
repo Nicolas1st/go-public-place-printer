56907578264c92b5ce64a44f43128f8672c2b449
Nicolas <nicolashereigo@gmail.com>
2022-05-11 16:32:37 +0300

    Moved out cookie relation code into cookie.go

--- api/auth/cookie.go
+++ api/auth/cookie.go
@@ -1,4 +1,29 @@
 package auth
 
+import (
+	"net/http"
+	"time"
+)
+
 const AuthCookieName string = "auth_session_cookie"
 const CookiePath string = "/"
+
+func SetAuthCookie(w http.ResponseWriter, r *http.Request, token string, expiryTime time.Time) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     AuthCookieName,
+		Value:    token,
+		Path:     CookiePath,
+		Expires:  expiryTime,
+		HttpOnly: true,
+	})
+}
+
+func RemoveAuthCookie(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     AuthCookieName,
+		Value:    "",
+		Path:     CookiePath,
+		Expires:  time.Now(),
+		HttpOnly: true,
+	})
+}

134318e441f3e69877ab838050bc6230aeba7ad6
Nicolas <nicolashereigo@gmail.com>
2022-05-11 20:48:59 +0300

    Added CreateAdminFunction

--- persistence/db/users.go
+++ persistence/db/users.go
@@ -1,6 +1,30 @@
 package db
 
-import "printer/persistence/model"
+import (
+	"fmt"
+	"printer/persistence/model"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (wrapper *Database) CreateAdmin(name, email, password string) error {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), 16)
+	if err != nil {
+		fmt.Println("Could not create the admin account")
+		return err
+	}
+
+	user := model.User{
+		Name:         name,
+		Email:        email,
+		PasswordHash: string(passwordHash),
+		Role:         model.Admin,
+	}
+
+	result := wrapper.db.Create(&user)
+
+	return result.Error
+}
 
 // CreateNewUser - creates new user, if it's not possible an error value is returned
 func (wrapper *Database) CreateNewUser(name, email, hashedPassword string) error {

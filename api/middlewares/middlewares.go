d9aa54a428c562ddb090abd40a3f28a0547560d3
Nicolas <nicolashereigo@gmail.com>
2022-05-11 16:18:32 +0300

    Moved out the types into separate file

---
+++ api/middlewares/middlewares.go
@@ -0,0 +1,7 @@
+package middlewares
+
+import "net/http"
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+type ApiRequestMiddleware func(func(http.ResponseWriter, *http.Request) error) http.HandlerFunc

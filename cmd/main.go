e0da22e5e8a480cef0548c9e048c2f4d0bec93b0
Nicolas <nicolashereigo@gmail.com>
2022-05-11 20:49:48 +0300

    Updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"printer/api/auth"
 	"printer/api/jobs"
@@ -18,11 +19,15 @@ import (
 func main() {
 	// config
 	server := &http.Server{Addr: "127.0.0.1:8880"}
-	dsn := "host=localhost user=postgres password=password dbname=stuff port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=password dbname=printer port=5432 sslmode=disable"
 
 	// persistence layer
 	db := db.NewDatabase(dsn)
 	jobq := jobq.NewJobQueue()
+
+	// create admin account
+	fmt.Println(db.CreateAdmin("admin", "admin@admin.admin", "admin"))
+
 	// filer := filer.NewFiler("../files", 2<<30)
 	filer := filer.NewFiler("/home/nicolas/Desktop/printer-app/files", 2<<32)
 	sessionStorage := session.NewSessionStorage()
@@ -31,19 +36,20 @@ func main() {
 	// init middlewares
 	requireAuth := middlewares.BuildRequireAuth(authHandlers.GetSessionIfValid, "/")
 	requireNotAuth := middlewares.BuildRequireNotAuth(authHandlers.GetSessionIfValid, "/submit-file")
+	onlyAdmin := middlewares.BuildOnlyAdmin("/")
 
 	// serving static files
 	files := http.FileServer(http.Dir("../web"))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 
 	// creating views
-	views := views.NewViews("../web/html")
+	views := views.NewViews("../web/html", db)
 
 	// authenication
-	loginRedirector := middlewares.BuildRedirectOnRequest("/", "/")
+	loginRedirector := middlewares.BuildRedirectOnRequest("/submit-file", "/login")
 	http.HandleFunc("/auth/login", requireNotAuth(loginRedirector(authHandlers.Login)))
 
-	logoutRedirector := middlewares.BuildRedirectOnRequest("/", "/")
+	logoutRedirector := middlewares.BuildRedirectOnRequest("/", "/submit-file")
 	http.HandleFunc("/auth/logout", requireAuth(logoutRedirector(authHandlers.Logout)))
 
 	// set up views
@@ -51,12 +57,13 @@ func main() {
 	http.HandleFunc("/login", requireNotAuth(views.Login))
 	http.HandleFunc("/signup", requireNotAuth(views.Signup))
 	http.HandleFunc("/submit-file", requireAuth(views.SubmitFile))
-	http.HandleFunc("/user-manager", requireAuth(views.UserManager))
+	http.HandleFunc("/user-manager", requireAuth(onlyAdmin(views.UserManager)))
+	http.HandleFunc("/profile", requireAuth(views.Profile))
 
 	// set up jobs api routes
 	jobsHandlers := jobs.NewJobsHandlers(jobq, filer)
 
-	RedirectOnJobResult := middlewares.BuildRedirectOnRequest("/submit-file", "/user-manager")
+	RedirectOnJobResult := middlewares.BuildRedirectOnRequest("/profile", "/submit-file")
 	http.HandleFunc("/jobs/submission", requireAuth(RedirectOnJobResult(jobsHandlers.SubmitJob)))
 	http.HandleFunc("/jobs/cancellation", requireAuth(RedirectOnJobResult(jobsHandlers.SubmitJob)))
 

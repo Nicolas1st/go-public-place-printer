d55d73afe4002c9b4832d08ae9ff99909f1d7ef6
Nicolas <nicolashereigo@gmail.com>
2022-05-11 16:18:09 +0300

    Refactored middlewares

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -3,7 +3,6 @@ package middlewares
 import (
 	"context"
 	"net/http"
-	"printer/api/auth"
 	"printer/persistence/model"
 )
 
@@ -11,41 +10,23 @@ var ContextSessionKey contextSessionKey = "contextSessionKey"
 
 type contextSessionKey string
 
-type SessionStorageInterface interface {
-	GetSessionByToken(sessionToken string) (*model.Session, error)
-}
-
-type Middleware func(http.HandlerFunc) http.HandlerFunc
-
-// BuildOnlyAuth - build middelware that
-// prevents non-authenticated users from making requests to certain enpoints (parameter next)
-// based on whether the auth cookie is present
-// and the validity of the session
-func BuildOnlyAuth(sessionStorage SessionStorageInterface, redirectTo http.HandlerFunc) Middleware {
+// RequireAuth - builds middelware that
+// prevents non-authenticated users from making requests to `next` handler
+// protected by this middleware
+func BuildRequireAuth(
+	GetSessionIfValid func(w http.ResponseWriter, r *http.Request) (*model.Session, bool),
+	urlForNotAuthneticated string,
+) Middleware {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			// check if auth cookie is present
-			cookie, err := r.Cookie(auth.AuthCookieName)
-			if err != nil {
-				redirectTo(w, r)
-				return
+			session, isValid := GetSessionIfValid(w, r)
+			if isValid {
+				// add session to the context for furhter handlers
+				r = r.WithContext(context.WithValue(r.Context(), ContextSessionKey, session))
+				next(w, r)
+			} else {
+				http.Redirect(w, r, urlForNotAuthneticated, http.StatusSeeOther)
 			}
-
-			// checking whether a valid sesion exists for the provided
-			session, err := sessionStorage.GetSessionByToken(cookie.Value)
-			if err != nil {
-				redirectTo(w, r)
-				return
-			}
-
-			if session.IsExpired() {
-				redirectTo(w, r)
-				return
-			}
-
-			// storing the session in the context
-			r = r.WithContext(context.WithValue(r.Context(), ContextSessionKey, session))
-			next.ServeHTTP(w, r)
 		}
 	}
 }
@@ -53,17 +34,17 @@ func BuildOnlyAuth(sessionStorage SessionStorageInterface, redirectTo http.Handl
 // BuildOnlyNotAuth - builds middleware that
 // prevents authenticated users from making requests to certain enpoints (parameter next)
 // based on whether the auth cookie is present
-func BuildOnlyNotAuth(redirectTo http.HandlerFunc) Middleware {
+func BuildRequireNotAuth(
+	GetSessionIfValid func(w http.ResponseWriter, r *http.Request) (*model.Session, bool),
+	urlForAuthenticated string,
+) Middleware {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			_, err := r.Cookie(auth.AuthCookieName)
-
-			if err == nil {
-				// non authenticated don't have auth cookie
-				redirectTo(w, r)
+			_, isSessionValid := GetSessionIfValid(w, r)
+			if !isSessionValid {
+				next(w, r)
 			} else {
-				// authenticated users have one
-				next.ServeHTTP(w, r)
+				http.Redirect(w, r, urlForAuthenticated, http.StatusSeeOther)
 			}
 		}
 	}

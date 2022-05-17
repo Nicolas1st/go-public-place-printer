0ad6b3118081cb2e5176753935c7f9c1cb09ea26
Nicolas <nicolashereigo@gmail.com>
2022-05-17 12:42:05 +0300

    Fix

--- middlewares/middlewares.go
+++
@@ -1,9 +0,0 @@
-package middlewares
-
-import "net/http"
-
-type Middleware func(http.HandlerFunc) http.HandlerFunc
-type ApiRequestMiddleware func(func(http.ResponseWriter, *http.Request) error) http.HandlerFunc
-type contextSessionKey string
-
-var ContextSessionKey contextSessionKey = "contextSessionKey"

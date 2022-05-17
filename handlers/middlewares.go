68da2a672985eb9e52c42425a26fb5628674eb4b
Nicolas <nicolashereigo@gmail.com>
2022-05-17 17:17:29 +0300

    Fixed middlewares

--- handlers/middlewares.go
+++ handlers/middlewares.go
@@ -2,35 +2,42 @@ package handlers
 
 import "net/http"
 
-func ForNotLoggedIn(sessioner Sessioner, w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	_, ok := GetSession(sessioner, r)
-	if ok {
-		http.Redirect(w, r, DefaultEndpoints.PrinterPage, http.StatusSeeOther)
-	} else {
-		next(w, r)
+func ForNotLoggedIn(sessioner Sessioner, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, ok := GetSession(sessioner, r)
+		if ok {
+			http.Redirect(w, r, DefaultEndpoints.PrinterPage, http.StatusSeeOther)
+		} else {
+			next(w, r)
+		}
+
 	}
 }
 
-func ForCommonUsers(sessioner Sessioner, w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	_, ok := GetSession(sessioner, r)
-	if !ok {
-		http.Redirect(w, r, DefaultEndpoints.LoginPage, http.StatusSeeOther)
-	} else {
-		next(w, r)
+func ForCommonUsers(sessioner Sessioner, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, ok := GetSession(sessioner, r)
+		if !ok {
+			http.Redirect(w, r, DefaultEndpoints.LoginPage, http.StatusSeeOther)
+		} else {
+			next(w, r)
+		}
 	}
 }
 
-func ForAdmin(sessioner Sessioner, w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	session, ok := GetSession(sessioner, r)
-	if !ok {
-		http.Redirect(w, r, DefaultEndpoints.LoginPage, http.StatusSeeOther)
-		return
-	}
+func ForAdmin(sessioner Sessioner, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, ok := GetSession(sessioner, r)
+		if !ok {
+			http.Redirect(w, r, DefaultEndpoints.LoginPage, http.StatusSeeOther)
+			return
+		}
 
-	if !session.User.IsAdmin() {
-		http.Redirect(w, r, DefaultEndpoints.PrinterPage, http.StatusSeeOther)
-		return
-	}
+		if !session.User.IsAdmin() {
+			http.Redirect(w, r, DefaultEndpoints.PrinterPage, http.StatusSeeOther)
+			return
+		}
 
-	next(w, r)
+		next(w, r)
+	}
 }

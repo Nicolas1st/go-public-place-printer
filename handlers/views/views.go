f6f56420c59346d75f64d4cfabbba2130102b13c
Nicolas <nicolashereigo@gmail.com>
2022-05-17 17:14:45 +0300

    Fix

--- handlers/views/views.go
+++ handlers/views/views.go
@@ -14,7 +14,7 @@ type database interface {
 }
 
 type sessioner interface {
-	GetSessionByToken(sessionToken string) (*model.Session, error)
+	GetSessionByToken(sessionToken string) (*model.Session, bool)
 	StoreSession(session *model.Session) (string, time.Time)
 	RemoveSession(sessionToken string)
 }

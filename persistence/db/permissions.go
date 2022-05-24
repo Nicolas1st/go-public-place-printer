e95692f199b44c1ebad20e678e5b9135be3d6b49
Nicolas <nicolashereigo@gmail.com>
2022-05-24 11:44:37 +0300

    Changed return values for database functions

--- persistence/db/permissions.go
+++ persistence/db/permissions.go
@@ -2,47 +2,47 @@ package db
 
 import "printer/persistence/model"
 
-func (wrapper *Database) SetPagesPerMonth(userID uint, pagesPerMonth int) error {
+func (wrapper *Database) SetPagesPerMonth(userID uint, pagesPerMonth uint) (uint, error) {
 	user := model.User{}
 
 	if result := wrapper.db.First(&user, userID); result.Error != nil {
-		return result.Error
+		return user.PagesPerMonth, result.Error
 	}
 
 	user.PagesPerMonth = pagesPerMonth
 	if result := wrapper.db.Save(&user); result.Error != nil {
-		return result.Error
+		return user.PagesPerMonth, result.Error
 	}
 
-	return nil
+	return user.PagesPerMonth, nil
 }
 
-func (wrapper *Database) AllowUsingPrinter(userID uint) error {
+func (wrapper *Database) AllowUsingPrinter(userID uint) (bool, error) {
 	user := model.User{}
 
 	if result := wrapper.db.First(&user, userID); result.Error != nil {
-		return result.Error
+		return user.CanUsePrinter, result.Error
 	}
 
 	user.CanUsePrinter = true
 	if result := wrapper.db.Save(&user); result.Error != nil {
-		return result.Error
+		return user.CanUsePrinter, result.Error
 	}
 
-	return nil
+	return user.CanUsePrinter, nil
 }
 
-func (wrapper *Database) ForbidUsingPrinter(userID uint) error {
+func (wrapper *Database) ForbidUsingPrinter(userID uint) (bool, error) {
 	user := model.User{}
 
 	if result := wrapper.db.First(&user, userID); result.Error != nil {
-		return result.Error
+		return user.CanUsePrinter, result.Error
 	}
 
 	user.CanUsePrinter = false
 	if result := wrapper.db.Save(&user); result.Error != nil {
-		return result.Error
+		return user.CanUsePrinter, result.Error
 	}
 
-	return nil
+	return user.CanUsePrinter, nil
 }

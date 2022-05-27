02f94fc657f6866c38c5d7f24b52c5cc31946521
Nicolas <nicolashereigo@gmail.com>
2022-05-27 11:16:18 +0300

    Added fields to print model

--- persistence/model/print.go
+++ persistence/model/print.go
@@ -5,7 +5,9 @@ import "gorm.io/gorm"
 type Print struct {
 	gorm.Model
 	Filename      string
+	Filepath      string
 	NumberOfPages int
+	Username      string
 	UserID        uint
 	User          User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }

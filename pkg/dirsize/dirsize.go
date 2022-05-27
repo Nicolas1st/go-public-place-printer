dd913974b4840869d82386cb18f291235b06ec4b
Nicolas <nicolashereigo@gmail.com>
2022-05-27 09:56:15 +0300

    Updated dirsize code

--- pkg/dirsize/dirsize.go
+++ pkg/dirsize/dirsize.go
@@ -5,17 +5,33 @@ import (
 	"path/filepath"
 )
 
-func GetDirectorySizeInGB(path string) int64 {
-	var dirSize int64 = 0
-	readSize := func(path string, file os.FileInfo, err error) error {
-		if !file.IsDir() {
-			dirSize += file.Size()
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-		return nil
-	}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
 
-	filepath.Walk(path, readSize)
-	sizeGB := dirSize / 1024 / 1024 / 1024
-
-	return sizeGB
+	size = size / 1024 / 1024 / 1024
+	return size, err
 }
+
+// func GetDirectorySizeInGB(path string) int64 {
+// 	var dirSize int64 = 0
+// 	readSize := func(path string, file os.FileInfo, err error) error {
+// 		if !file.IsDir() {
+// 			dirSize += file.Size()
+// 		}
+// 		return nil
+// 	}
+
+// 	filepath.Walk(path, readSize)
+// 	sizeGB := dirSize / 1024 / 1024 / 1024
+
+// 	return sizeGB
+// }

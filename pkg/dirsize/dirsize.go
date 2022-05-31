40cf00da6072d139d5fd4fc0801ae7083966fcb1
Nicolas <nicolashereigo@gmail.com>
2022-05-31 09:29:36 +0300

    Removed comments

--- pkg/dirsize/dirsize.go
+++ pkg/dirsize/dirsize.go
@@ -20,18 +20,3 @@ func DirSize(path string) (int64, error) {
 	size = size / 1024 / 1024 / 1024
 	return size, err
 }
-
-// func GetDirectorySizeInGB(path string) int64 {
-// 	var dirSize int64 = 0
-// 	readSize := func(path string, file os.FileInfo, err error) error {
-// 		if !file.IsDir() {
-// 			dirSize += file.Size()
-// 		}
-// 		return nil
-// 	}
-
-// 	filepath.Walk(path, readSize)
-// 	sizeGB := dirSize / 1024 / 1024 / 1024
-
-// 	return sizeGB
-// }
